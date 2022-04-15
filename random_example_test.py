6cdf2ff1fd78a20415e260701e7d99b9bdff2b8c
mleming <matthew.j.leming@gmail.com>
2022-04-15 20:04:59 +0300

    Updated recursive error

--- random_example_test.py
+++ random_example_test.py
@@ -17,9 +17,9 @@ confounds = np.random.rand(2, N)
 # floats (i.e., continuous covariates) and strings (i.e., discrete, or labeled,
 # covariates), it is necessary to cast them all as objects. Slows things down,
 # but if covariates are all of one type, they can be cast as one or the other.
-c1 = np.array([norm(1, 2, N/3),norm(0.5, 2, N/3),norm(2, 1, N/3)],dtype=object)
-c2 = np.array([norm(3, 2, N/3),norm(0.5, 2, N/3),norm(3.1, 0.25, N/3)],dtype=object)
-c3 = np.array([norm(2, 1, N/3),norm(0.5, 2, N/3),norm(2.9, 1, N/3)],dtype=object)
+c1 = np.array([norm(1, 2, int(N/3)),norm(0.5, 2, int(N/3)),norm(2, 1, int(N/3))],dtype=object)
+c2 = np.array([norm(3, 2, int(N/3)),norm(0.5, 2, int(N/3)),norm(3.1, 0.25, int(N/3))],dtype=object)
+c3 = np.array([norm(2, 1, int(N/3)),norm(0.5, 2, int(N/3)),norm(2.9, 1, int(N/3))],dtype=object)
 confounds = np.concatenate((c1,c2,c3),axis=1)
 c4 = np.array([[np.random.choice(["a","b","c"]) for x in range(N)]],dtype=object)
 confounds = np.concatenate((confounds,c4),axis=0)
@@ -28,8 +28,8 @@ classes = np.array([0 for x in range(int(N/2))] + [1 for x in range(int(N/2))])
 classes = np.array([0 for x in range(int(N/3))] + [1 for x in range(int(N/3))] + [2 for x in range(int(N/3))])
 
 selection = class_balance(classes,confounds,plim=0.25)
-print np.sum(selection)
-print test_all(classes[selection],confounds[:,selection])
+print(np.sum(selection))
+print(test_all(classes[selection],confounds[:,selection]))
 
 cnum = 0
 no_bins = 50
